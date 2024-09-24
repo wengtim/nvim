f7c354e5ce1ee40f9c06d8e2c28adfe7e1cbe80b
wengtim <wengtim05@proton.me>
2024-09-24 20:41:42 +0800

    added find keymaps

--- lua/user/lazy/telescope.lua
+++ lua/user/lazy/telescope.lua
@@ -10,6 +10,7 @@ return {
         local builtin = require('telescope.builtin')
         vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
         vim.keymap.set('n', '<leader>fg', builtin.git_files, {})
+        vim.keymap.set('n', '<leader>fk', builtin.keymaps, {})
         vim.keymap.set('n', '<leader>fw', function()
             builtin.grep_string({ search = vim.fn.input("Grep > ") })
         end)
