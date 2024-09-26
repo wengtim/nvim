6a8004d25b28985ff3b375df5a56a9e4037e8b75
wengtim <wengtim05@proton.me>
2024-09-26 14:24:43 +0800

    haha

--- lua/user/lazy/copilot.lua
+++ lua/user/lazy/copilot.lua
@@ -10,20 +10,20 @@ return {
         end,
     },
 
-    {
-        "codota/tabnine-nvim",
-        build = "./dl_binaries.sh",
-        config = function()
-            require('tabnine').setup({
-                disable_auto_comment = true,
-                accept_keymap = "<Tab>",
-                dismiss_keymap = "<C-c>",
-                debounce_ms = 800,
-                suggestion_color = { gui = "#808080", cterm = 244 },
-                exclude_filetypes = { "TelescopePrompt" },
-                log_file_path = nil,
-            })
-            vim.cmd("TabnineDisable")
-        end,
-    },
+    -- {
+    --     "codota/tabnine-nvim",
+    --     build = "./dl_binaries.sh",
+    --     config = function()
+    --         require('tabnine').setup({
+    --             disable_auto_comment = true,
+    --             accept_keymap = "<Tab>",
+    --             dismiss_keymap = "<C-c>",
+    --             debounce_ms = 800,
+    --             suggestion_color = { gui = "#808080", cterm = 244 },
+    --             exclude_filetypes = { "TelescopePrompt" },
+    --             log_file_path = nil,
+    --         })
+    --         vim.cmd("TabnineDisable")
+    --     end,
+    -- },
 }
