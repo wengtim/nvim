7c1bf07f67d3e7700cdeef147e1d80c6c4eca60b
wengtim <wengtim05@proton.me>
2024-09-29 11:16:20 +0800

    added vim-with-me

---
+++ lua/user/lazy/theprimeagen.lua
@@ -0,0 +1,16 @@
+return {
+    {
+        'theprimeagen/vim-be-good',
+
+        dependencies = {
+            "nvim-lua/plenary.nvim"
+        },
+
+        config = function() end
+    },
+
+    {
+        "theprimeagen/vim-with-me",
+        config = function() end
+    },
+}
