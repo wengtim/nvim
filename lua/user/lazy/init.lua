be7d91c7ce613cc0093d8324ca595296f0b08e37
wengtim <wengtim05@proton.me>
2024-09-30 19:26:31 +0800

    added tmux navigator

--- lua/user/lazy/init.lua
+++ lua/user/lazy/init.lua
@@ -1,18 +1,28 @@
 return {
 
-    {
-        "nvim-lua/plenary.nvim",
-        name = "plenary"
-    },
+   {
+      "nvim-lua/plenary.nvim",
+      name = "plenary"
+   },
 
-    {
-        "eandrju/cellular-automaton.nvim"
-    },
+   {
+      "eandrju/cellular-automaton.nvim"
+   },
 
-    {
-        'windwp/nvim-autopairs',
-        event = "InsertEnter",
-        config = true,
-    },
+   {
+      'windwp/nvim-autopairs',
+      event = "InsertEnter",
+      config = true,
+   },
+
+   {
+      "christoomey/vim-tmux-navigator",
+      lazy = false,
+
+      vim.keymap.set("n", "<C-h>", "<cmd>TmuxNavigateLeft<CR>", {desc = "Tmux navigate left"}),
+      vim.keymap.set("n", "<C-j>", "<cmd>TmuxNavigateDown<CR>", {desc = "Tmux navigate down"}),
+      vim.keymap.set("n", "<C-k>", "<cmd>TmuxNavigateUp<CR>", {desc = "Tmux navigate up"}),
+      vim.keymap.set("n", "<C-l>", "<cmd>TmuxNavigateRight<CR>", {desc = "Tmux navigate right"}),
+   },
 }
 
