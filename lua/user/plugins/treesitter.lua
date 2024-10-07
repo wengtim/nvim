7bbbd48cde441785e53c856127ee9c38468ded86
wengtim <wengtim05@proton.me>
2024-10-07 21:24:49 +0800

    new: added fugitive

--- lua/user/plugins/treesitter.lua
+++ lua/user/plugins/treesitter.lua
@@ -1,59 +1,59 @@
 return {
-    "nvim-treesitter/nvim-treesitter",
+   "nvim-treesitter/nvim-treesitter",
 
-    dependencies = {
-        "nvim-treesitter/playground",
-    },
+   dependencies = {
+      "nvim-treesitter/playground",
+   },
+   build = ":TSUpdate",
 
-    -- build = ":TSUpdate",
-    config = function()
-        require("nvim-treesitter.configs").setup({
-            playground = {
-                enable = true,
-                updatetime = 25,
-                persist_queries = false,
-            },
-            ensure_installed = {
-                "c",
-                "css",
-                "vimdoc",
-                "javascript",
-                "typescript",
-                "lua",
-                "jsdoc",
-                "bash",
-                "go",
-                "html",
-                "python",
-                "sql",
-                "markdown",
-            },
+   config = function()
+      require("nvim-treesitter.configs").setup({
+         playground = {
+            enable = true,
+            updatetime = 25,
+            persist_queries = false,
+         },
+         ensure_installed = {
+            "c",
+            "css",
+            "vimdoc",
+            "javascript",
+            "typescript",
+            "lua",
+            "jsdoc",
+            "bash",
+            "go",
+            "html",
+            "python",
+            "sql",
+            "markdown",
+         },
 
-            sync_install = false,
+         sync_install = false,
 
-            auto_install = true,
+         auto_install = true,
 
-            indent = {
-                enable = true
-            },
+         indent = {
+            enable = true
+         },
 
-            highlight = {
-                enable = true,
-                additional_vim_regex_highlighting = false,
-            },
-        })
+         highlight = {
+            enable = true,
+            additional_vim_regex_highlighting = false,
+         },
+      })
 
-        local treesitter_parser_config = require("nvim-treesitter.parsers").get_parser_configs()
-        treesitter_parser_config.templ = {
-            install_info = {
-                url = "https://github.com/vrischmann/tree-sitter-templ.git",
-                files = {"src/parser.c", "src/scanner.c"},
-                branch = "master",
-            },
-        }
+      local treesitter_parser_config = require("nvim-treesitter.parsers").get_parser_configs()
+      treesitter_parser_config.templ = {
+         install_info = {
+            url = "https://github.com/vrischmann/tree-sitter-templ.git",
+            files = { "src/parser.c", "src/scanner.c" },
+            branch = "master",
+         },
+      }
 
-        vim.treesitter.language.register("templ", "templ")
+      vim.treesitter.language.register("templ", "templ")
 
-        vim.keymap.set("n", "<leader>t", "<cmd>TSHighlightCapturesUnderCursor<CR>", { desc = "Find type under cursor" })
-    end
+      vim.keymap.set("n", "<leader>t", "<cmd>TSHighlightCapturesUnderCursor<CR>", { desc = "Find type under cursor" })
+   end
 }
