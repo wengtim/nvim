e8d48be1c2fde5cd87056debf34d61fbf98ecb74
wengtim <wengtim05@proton.me>
2024-10-07 17:45:42 +0800

    delete some lsp dk why got prob

--- lua/user/lazy/lsp.lua
+++ lua/user/lazy/lsp.lua
@@ -1,115 +1,115 @@
 return {
-    "neovim/nvim-lspconfig",
-    dependencies = {
-        "williamboman/mason.nvim",
-        "williamboman/mason-lspconfig.nvim",
-        "jay-babu/mason-null-ls.nvim",
-        "hrsh7th/cmp-nvim-lsp",
-        "hrsh7th/cmp-buffer",
-        "hrsh7th/cmp-path",
-        "hrsh7th/cmp-cmdline",
-        "hrsh7th/nvim-cmp",
-        "L3MON4D3/LuaSnip",
-        "saadparwaiz1/cmp_luasnip",
-        "j-hui/fidget.nvim",
-    },
-    config = function()
-        local cmp = require('cmp')
-        local cmp_lsp = require("cmp_nvim_lsp")
-        local capabilities = vim.tbl_deep_extend(
-            "force",
-            {},
-            vim.lsp.protocol.make_client_capabilities(),
-            cmp_lsp.default_capabilities())
-        require("fidget").setup({
-            notification = { window = { winblend = 0 } },
-        })
-        require("mason").setup()
-        require("mason-lspconfig").setup({
-            ensure_installed = {
-                "clangd",
-                "cssls",
-                "emmet_ls",
-                "gopls",
-                "html",
-                "jdtls",
-                "jsonls",
-                "lua_ls",
-                "marksman",
-                "phpactor",
-                "rust_analyzer",
-                "sqls",
-                "ts_ls",
-                "vimls",
-                "zls",
-            },
-            handlers = {
-                function(server_name)
-                    require("lspconfig")[server_name].setup {
-                        capabilities = capabilities
-                    }
-                end,
-
-                ["lua_ls"] = function()
-                    local lspconfig = require("lspconfig")
-                    lspconfig.lua_ls.setup {
-                        capabilities = capabilities,
-                        settings = {
-                            Lua = {
-                                runtime = { version = "Lua 5.1" },
-                                diagnostics = {
-                                    globals = { "bit", "vim", "it", "describe", "before_each", "after_each" },
-                                }
-                            }
-                        }
-                    }
-                end,
-            }
-        })
-
-        -- Colorscheme problem
-        require("lspconfig").jdtls.setup({
-            on_attach = function(client)
-                client.server_capabilities.semanticTokensProvider = nil
-            end
-        })
-
-        -- Snippet
-        cmp.setup({
-            snippet = {
-                expand = function(args)
-                    require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
-                end,
-            },
-
-            mapping = cmp.mapping.preset.insert({
-                ['<Enter>'] = cmp.mapping.confirm({ select = true }),
-                ["<C-Space>"] = cmp.mapping.complete(),
-            }),
-            sources = cmp.config.sources({
-                { name = 'nvim_lsp' },
-                { name = 'luasnip' }, -- For luasnip users.
-            }, {
-                    { name = 'buffer' },
-                })
-        })
-
-        vim.diagnostic.config({
-            update_in_insert = false,
-            float = {
-                focusable = false,
-                style = "minimal",
-                border = "rounded",
-                source = "always",
-                header = "",
-                prefix = "",
-            },
-        })
-        vim.api.nvim_create_autocmd("FileType", {
-            pattern = {"html", "css"},
-            callback = function()
-                vim.b.format_on_save = false
+   "neovim/nvim-lspconfig",
+   dependencies = {
+      "williamboman/mason.nvim",
+      "williamboman/mason-lspconfig.nvim",
+      "jay-babu/mason-null-ls.nvim",
+      "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-buffer",
+      "hrsh7th/cmp-path",
+      "hrsh7th/cmp-cmdline",
+      "hrsh7th/nvim-cmp",
+      "L3MON4D3/LuaSnip",
+      "saadparwaiz1/cmp_luasnip",
+      "j-hui/fidget.nvim",
+   },
+   config = function()
+      local cmp = require('cmp')
+      local cmp_lsp = require("cmp_nvim_lsp")
+      local capabilities = vim.tbl_deep_extend(
+         "force",
+         {},
+         vim.lsp.protocol.make_client_capabilities(),
+         cmp_lsp.default_capabilities())
+      require("fidget").setup({
+         notification = { window = { winblend = 0 } },
+      })
+      require("mason").setup()
+      require("mason-lspconfig").setup({
+         ensure_installed = {
+            "clangd",
+            "cssls",
+            "emmet_ls",
+            "gopls",
+            "html",
+            "jdtls",
+            "jsonls",
+            "lua_ls",
+            "marksman",
+            "phpactor",
+            "rust_analyzer",
+            "sqls",
+            "ts_ls",
+            "vimls",
+            "zls",
+         },
+         handlers = {
+            function(server_name)
+               require("lspconfig")[server_name].setup {
+                  capabilities = capabilities
+               }
             end,
-        })
-    end
+
+            ["lua_ls"] = function()
+               local lspconfig = require("lspconfig")
+               lspconfig.lua_ls.setup {
+                  capabilities = capabilities,
+                  settings = {
+                     Lua = {
+                        runtime = { version = "Lua 5.1" },
+                        diagnostics = {
+                           globals = { "bit", "vim", "it", "describe", "before_each", "after_each" },
+                        }
+                     }
+                  }
+               }
+            end,
+         }
+      })
+
+      -- Colorscheme problem
+      require("lspconfig").jdtls.setup({
+         on_attach = function(client)
+            client.server_capabilities.semanticTokensProvider = nil
+         end
+      })
+
+      -- Snippet
+      cmp.setup({
+         snippet = {
+            expand = function(args)
+               require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+            end,
+         },
+
+         mapping = cmp.mapping.preset.insert({
+            ['<Enter>'] = cmp.mapping.confirm({ select = true }),
+            ["<C-Space>"] = cmp.mapping.complete(),
+         }),
+         sources = cmp.config.sources({
+            { name = 'nvim_lsp' },
+            { name = 'luasnip' }, -- For luasnip users.
+         }, {
+               { name = 'buffer' },
+            })
+      })
+
+      vim.diagnostic.config({
+         update_in_insert = false,
+         float = {
+            focusable = false,
+            style = "minimal",
+            border = "rounded",
+            source = "always",
+            header = "",
+            prefix = "",
+         },
+      })
+      vim.api.nvim_create_autocmd("FileType", {
+         pattern = {"html", "css"},
+         callback = function()
+            vim.b.format_on_save = false
+         end,
+      })
+   end
 }
