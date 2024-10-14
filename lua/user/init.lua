3d8e30a3d90092961418f57bf7df73809ea7fec9
wengtim <wengtim05@proton.me>
2024-10-14 15:22:20 +0800

    new: added diagnostic go next and prev

--- lua/user/init.lua
+++ lua/user/init.lua
@@ -39,29 +39,19 @@ autocmd('LspAttach', {
    group = user,
    callback = function(e)
       local opts = { buffer = e.buf }
-      vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
-      vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
-      vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
-      vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
-      vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
-      vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
-      vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
       vim.keymap.set("n", "<C-s>", function() vim.lsp.buf.signature_help() end, opts)
+      vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
+      vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
+      vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
+      vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
+      vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
+      vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
+      vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
+      vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
+      vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
    end
 })
 
--- autocmd('VimEnter', {
---     group = augroup("LazyAutocmdGroup", { clear = true }),
---     callback = function()
---         vim.defer_fn(function()
---             vim.cmd("Lazy update")
---             vim.cmd("Lazy sync")
---             vim.cmd("Lazy check")
---             vim.cmd("Lazy clean")
---         end, 100)
---     end,
--- })
-
-vim.g.netrw_browse_split = 0
 vim.g.netrw_winsize = 25
 vim.g.netrw_banner = 0
+vim.g.netrw_browse_split = 0
