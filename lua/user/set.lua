b22de7eebac35382db7ffb15275f1953ccae2d82
wengtim <wengtim05@proton.me>
2025-07-04 16:21:02 +0800

    changes: default indentation

--- lua/user/set.lua
+++ lua/user/set.lua
@@ -1,9 +1,9 @@
 vim.opt.nu = true
 vim.opt.relativenumber = true
 
-vim.opt.tabstop = 3
-vim.opt.softtabstop = 3
-vim.opt.shiftwidth = 3
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
 
 vim.opt.smartindent = true
