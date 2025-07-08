64065dcdb605ba4dc3caac008dc13415d51ff8c5
wengtim <wengtim05@proton.me>
2025-07-08 09:27:55 +0800

    changes: diagnostics shortcut key change

--- lua/user/init.lua
+++ lua/user/init.lua
@@ -46,8 +46,8 @@ autocmd('LspAttach', {
       vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
       vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
       vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
-      vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
-      vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
+      vim.keymap.set("n", "]d", function() vim.diagnostic.goto_next() end, opts)
+      vim.keymap.set("n", "[d", function() vim.diagnostic.goto_prev() end, opts)
       vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
    end
 })
