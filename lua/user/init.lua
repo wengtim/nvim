b558c66c3c5002010fe3e97c4c1be1d61f81815c
wengtim <wengtim05@proton.me>
2026-05-05 18:50:44 +0800

    feat: added vscode configurations

--- lua/user/init.lua
+++ lua/user/init.lua
@@ -1,57 +1,63 @@
-require("user.remap")
-require("user.set")
-require("user.lazy_init")
+if vim.g.vscode then
+  require("user.vscode_keymaps")
 
-local augroup = vim.api.nvim_create_augroup
-local user = augroup('user', {})
+else
+  require("user.remap")
+  require("user.set")
+  require("user.lazy_init")
 
-local autocmd = vim.api.nvim_create_autocmd
-local yank_group = augroup('HighlightYank', {})
+  local augroup = vim.api.nvim_create_augroup
+  local user = augroup('user', {})
 
-function R(name)
-   require("plenary.reload").reload_module(name)
+  local autocmd = vim.api.nvim_create_autocmd
+  local yank_group = augroup('HighlightYank', {})
+
+  function R(name)
+     require("plenary.reload").reload_module(name)
+  end
+
+  vim.filetype.add({
+     extension = {
+        templ = 'templ',
+     }
+  })
+
+  autocmd('TextYankPost', {
+     group = yank_group,
+     pattern = '*',
+     callback = function()
+        vim.highlight.on_yank({
+           higroup = 'IncSearch',
+           timeout = 40,
+        })
+     end,
+  })
+
+  autocmd({ "BufWritePre" }, {
+     group = user,
+     pattern = "*",
+     command = [[%s/\s\+$//e]],
+  })
+
+  autocmd('LspAttach', {
+     group = user,
+     callback = function(e)
+        local opts = { buffer = e.buf }
+        vim.keymap.set("n", "<C-s>", function() vim.lsp.buf.signature_help() end, opts)
+        vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
+        vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
+        vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
+        vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
+        vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
+        vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
+        vim.keymap.set("n", "]d", function() vim.diagnostic.goto_next() end, opts)
+        vim.keymap.set("n", "[d", function() vim.diagnostic.goto_prev() end, opts)
+        vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+     end
+  })
+
+  vim.g.netrw_winsize = 25
+  vim.g.netrw_banner = 0
+  vim.g.netrw_browse_split = 0
+
 end
-
-vim.filetype.add({
-   extension = {
-      templ = 'templ',
-   }
-})
-
-autocmd('TextYankPost', {
-   group = yank_group,
-   pattern = '*',
-   callback = function()
-      vim.highlight.on_yank({
-         higroup = 'IncSearch',
-         timeout = 40,
-      })
-   end,
-})
-
-autocmd({ "BufWritePre" }, {
-   group = user,
-   pattern = "*",
-   command = [[%s/\s\+$//e]],
-})
-
-autocmd('LspAttach', {
-   group = user,
-   callback = function(e)
-      local opts = { buffer = e.buf }
-      vim.keymap.set("n", "<C-s>", function() vim.lsp.buf.signature_help() end, opts)
-      vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
-      vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
-      vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
-      vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
-      vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
-      vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
-      vim.keymap.set("n", "]d", function() vim.diagnostic.goto_next() end, opts)
-      vim.keymap.set("n", "[d", function() vim.diagnostic.goto_prev() end, opts)
-      vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
-   end
-})
-
-vim.g.netrw_winsize = 25
-vim.g.netrw_banner = 0
-vim.g.netrw_browse_split = 0
---
+++ lua/user/vscode_keymaps.lua
@@ -0,0 +1,28 @@
+vim.keymap.set("v", "<", "<gv")
+vim.keymap.set("v", ">", ">gv")
+
+vim.keymap.set("n", "<leader>ee", vim.cmd.Ex, { desc = "Open File Explorer" })
+
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { desc = "Move down" })
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { desc = "Move up" })
+vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]], { desc = "Search and replace" })
+
+vim.keymap.set("n", "J", "mzJ`z", { desc = "Join lines" })
+vim.keymap.set("n", "G", "Gzz", { desc = "End of page and center" })
+vim.keymap.set("n", "<C-d>", "<C-d>zz", { desc = "Page down" })
+vim.keymap.set("n", "<C-u>", "<C-u>zz", { desc = "Page up" })
+vim.keymap.set("n", "n", "nzzzv", { desc = "Keep cursor centered during search" })
+vim.keymap.set("n", "N", "Nzzzv", { desc = "Keep cursor centered during search" })
+
+vim.keymap.set("n", "el", "$", { desc = "End of line" })
+vim.keymap.set("n", "yel", "y$", { desc = "Yank End of line" })
+vim.keymap.set("n", "vel", "v$", { desc = "Select End of line" })
+vim.keymap.set("n", "del", "d$", { desc = "Delete End of line" })
+
+vim.keymap.set("n", "<leader>d", "dt", { desc = "Delete till" })
+
+vim.keymap.set("x", "<leader>p", [["_dP]])
+
+vim.keymap.set("n", "Q", "<nop>", { desc = "No operation" })
+vim.keymap.set("n", "==", vim.lsp.buf.format, { desc = "Format code" })
+vim.keymap.set("n", "U", "<C-r>", { desc = "Redo" })
